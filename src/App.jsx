5e955e7688b84df16be773bb1deb3d2462de6174
Alexandrite22 <alex.hewson.1122@gmail.com>
2025-09-25 11:26:00 -0600

    feat: added basic light/dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { ChakraProvider, Box, Button } from '@chakra-ui/react'
+import { Box, Button, useColorMode, IconButton, useColorModeValue } from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue('gray.100', 'gray.700')
   return (
-    <ChakraProvider>
-      <Box p={4} bg='gray.100' maxW='md' mx='auto' mt={5}>
+      <Box position="relative" 
+        m="3rem" 
+        p="3rem" 
+        bg={bg} 
+        height={`calc(100vh - 6rem)`}
+        borderRadius="md" 
+        boxShadow="md" 
+        display="flex" 
+        flexDirection="column" 
+        flexGrow={1}
+        alignItems="center" 
+        justifyContent="center" 
+        gap="1rem">
+        <IconButton
+          aria-label="Toggle theme"
+          icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          onClick={toggleColorMode}
+          position="absolute"
+          top="1rem"
+          right="1rem"
+        />
         <Button colorScheme='teal' size='lg'>
           Click Me
         </Button>
       </Box>
-    </ChakraProvider>
   )
 }
 
